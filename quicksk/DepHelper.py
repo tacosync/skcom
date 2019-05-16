1164e958b00c964b3c85588a9a01887d8c4383b4
Raymond Wu (小璋丸) <raymondwu@fontrip.com>
2019-05-16 17:06:02 +0800

    完成輔助安裝程式
    Mentions: #5

--- quicksk/DepHelper.py
+++ quicksk/DepHelper.py
@@ -1,22 +1,34 @@
 import os.path
 import re
 import subprocess
+import time
 import winreg
 import zipfile
 
 import comtypes.client
 import requests
+from packaging import version
+import win32pipe
+import pywintypes
 
-# 用 Power Shell 執行一個指令
+## 用 Power Shell 執行一個指令
+#  * 可以使用系統管理者執行
+#  * 參數含有空白時會發生問題, 例如 tasklist 的 /fi
 def ps_exec(cmd, admin=False):
-    tokens = cmd.split(' ')
+    if isinstance(cmd, list):
+        tokens = cmd
+    else:
+        tokens = cmd.split(' ')
     prog = tokens[0]
-    args = '"%s"' % ' '.join(tokens[1:])
     spcmd = [
         'powershell.exe', 'Start-Process',
-        '-FilePath', prog,
-        '-ArgumentList', args
+        '-FilePath', prog
     ]
+    if len(tokens) > 1:
+        quoted_tokens = list(map(lambda i: '"%s"' % i, tokens[1:]))
+        args = ','.join(quoted_tokens)
+        spcmd.append('-ArgumentList')
+        spcmd.append(args)
 
     # 用系統管理員身分執行
     if admin:
@@ -34,44 +46,75 @@ def ps_exec(cmd, admin=False):
     else:
         return None
 
-## 檢查 Visual Studio 2010 可轉發套件是否已安裝
-# TODO: 目前檢查版本有缺陷, 等之後明朗化再實作
-def check_vsredist():
+## 用 cmd 執行一個指令
+#  * 適用指令較廣泛
+#  * 無法使用系統管理者身分執行
+def cmd_exec(cmd):
+    if isinstance(cmd, list):
+        tokens = cmd
+    else:
+        tokens = cmd.split(' ')
+
+    spcmd = ['cmd', '/C'] + tokens
+    completed = subprocess.run(spcmd, capture_output=True)
+    if completed.returncode == 0:
+        # 因為被 cmd 包了一層, 不管怎樣都是 return 0
+        return completed.stdout.decode('cp950')
+    else:
+        return None
+
+## 取得 Visual C++ 2010 可轉發套件版本資訊
+def verof_vcredist():
     try:
+        # 版本字串格式: v10.0.40219.325
         keyname = r'SOFTWARE\WOW6432Node\Microsoft\VisualStudio\10.0\VC\VCRedist\x64'
         key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, keyname)
         pkg_ver = winreg.QueryValueEx(key, 'Version')[0].strip('v')
+        if not re.match(r'(\d+\.){3}\d+', pkg_ver):
+            pkg_ver = '0.0.0.0'
     except FileNotFoundError:
         pkg_ver = '0.0.0.0'
 
-    return pkg_ver
+    return version.parse(pkg_ver)
 
 ## 安裝 Visual C++ 2010 x64 Redistributable 10.0.40219.325
-def install_vsredist():
+def install_vcredist():
+    # 下載與安裝
     url = 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe'
     vcdist = download_file(url, check_dir('~/.skcom'))
-    ps_exec(vcdist + ' setup /passive', admin=True)
+    ps_exec(vcdist + r' setup /passive', admin=True)
+
+    # 等待安裝完成
+    args = ['tasklist', '/fi', 'imagename eq vcredist_x64.exe', '/fo', 'csv']
+    output = cmd_exec(args)
+    while output.count('\r\n') == 2:
+        time.sleep(0.5)
+        output = cmd_exec(args)
+
+    # 移除安裝包
     os.remove(vcdist)
-    return True
 
 ## 檢查群益 API 元件是否已註冊
-def check_skcom():
+def verof_skcom():
     cmd = r'reg query HKLM\SOFTWARE\Classes\TypeLib /s /f SKCOM.dll'
     result = ps_exec(cmd)
 
-    version = '0.0.0.0'
+    skcom_ver = '0.0.0.0'
     if result is not None:
         lines = result.split('\r\n')
         for line in lines:
             # 找 DLL 檔案路徑
             m = re.match(r'.+REG_SZ\s+(.+SKCOM.dll)', line)
             if m is not None:
-                dll_path = m.group(1)
                 # 取檔案摘要內容裡版本號碼
+                dll_path = m.group(1)
                 fso = comtypes.client.CreateObject('Scripting.FileSystemObject')
-                version = fso.GetFileVersion(dll_path)
+                try:
+                    skcom_ver = fso.GetFileVersion(dll_path)
+                except:
+                    pass
 
-    return version
+    return version.parse(skcom_ver)
 
 ## 安裝群益 API 元件
 def install_skcom():
--- setup.py
+++ setup.py
@@ -2,23 +2,28 @@ import quicksk.DepHelper
 from packaging import version
 
 def main():
-    not_installed = version.parse('0.0.0.0')
+    # 檢查與更新 Visual C++ 2010 (>= 10.0.40219.325)
+    required_ver = version.parse('10.0.40219.325')
+    current_ver = quicksk.DepHelper.verof_vcredist()
+    if current_ver < required_ver:
+        print('安裝 Visual C++ 2010 可轉發套件')
+        quicksk.DepHelper.install_vcredist()
+        current_ver = quicksk.DepHelper.verof_vcredist()
+        if current_ver < required_ver:
+            print('安裝失敗, 可能是您取消了安裝動作')
+            return
+
+    print('Visual C++ 2010 可轉發套件已安裝, 版本:', current_ver)
+
+    # 檢查與更新 API 元件 (>= 2.13.11)
     required_ver = version.parse('2.13.11')
-    current_ver = version.parse(quicksk.DepHelper.check_skcom())
-    if current_ver >= required_ver:
-        print('群益 API 元件已註冊, 版本:', current_ver)
-    else:
-        if current_ver == not_installed:
-            print('群益 API 元件未安裝, 現在為您安裝')
-        else:
-            print('群益 API 元件版本太舊, 現在為您更新')
+    current_ver = quicksk.DepHelper.verof_skcom()
+    if current_ver < required_ver:
+        print('安裝與註冊群益 API 元件')
         quicksk.DepHelper.install_skcom()
-        current_ver = version.parse(quicksk.DepHelper.check_skcom())
-        print('安裝完成, 版本:', current_ver)
+        current_ver = quicksk.DepHelper.verof_skcom()
+
+    print('群益 API 元件已安裝, 版本:', current_ver)
 
 if __name__ == '__main__':
     main()
-    # print(quicksk.DepHelper.check_vsredist())
-    # quicksk.DepHelper.install_vsredist()
-    # print(quicksk.DepHelper.check_skcom())
-    # quicksk.DepHelper.install_skcom()
