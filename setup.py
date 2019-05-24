3a0b0d98c2e7c8df680fbfd532b39034bf385835
Raymond Wu (小璋丸) <raymondwu@fontrip.com>
2019-05-24 16:45:44 +0800

    setup.py 設定
    Mentions: #7

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import twnews.common as common
+
+# Load reStructedText description.
+# Online Editor   - http://rst.ninjs.org/
+# Quick Reference - http://docutils.sourceforge.net/docs/user/rst/quickref.html
+readme = open('README.rst', 'r')
+longdesc = readme.read()
+readme.close()
+
+# See
+# https://packaging.python.org/tutorials/packaging-projects/
+# https://python-packaging.readthedocs.io/en/latest/non-code-files.html
+setup(
+    name='skcom',
+    version='0.1.0',
+    description='群益證券聽牌套件',
+    long_description=longdesc,
+    packages=find_packages(),
+    url='https://github.com/virus-warnning/skcom',
+    license='MIT',
+    author='Raymond Wu',
+    package_data={
+        'twnews': ['conf/*', 'samples/*']
+    },
+    install_requires=[
+        'comtypes',
+        'packaging',
+        'pywin32',
+        'requests'
+    ],
+    classifiers=[
+        'Operating System :: Microsoft :: Windows :: Windows 10'
+    ],
+    python_requires='>=3.5'
+)
