fd41469d5d39cca4f57a5129c7c6ad8554cd269f
Raymond Wu (小璋丸) <virus.warnning@gmail.com>
2019-10-06 21:58:29 +0800

    logging 設定檔改善
    * 改用 yaml 設定
    * 移除 ini 設定
    * 自動替換 ~ 目錄
    * log 檔目錄不存在時, 自動建立目錄
    
    Mentions: #36 #39

--- skcom/__init__.py
+++ skcom/__init__.py
@@ -6,6 +6,21 @@ skcom
 import os.path
 import logging.config
 
-cfg_file = os.path.dirname(__file__) + '/conf/logging.ini'
-logging.config.fileConfig(cfg_file)
-logger = logging.getLogger('skcom')
+import yaml
+
+cfg_yaml = '{}/conf/logging.yaml'.format(os.path.dirname(__file__))
+with open(cfg_yaml, 'r') as cfg_file:
+    cfg_dict = yaml.load(open(cfg_yaml, 'r'), Loader=yaml.SafeLoader)
+
+    for name in cfg_dict['handlers']:
+        handler = cfg_dict['handlers'][name]
+        if 'filename' in handler:
+            # Replace ~ (home) as absolute path.
+            if handler['filename'].startswith('~'):
+                handler['filename'] = os.path.expanduser(handler['filename'])
+            # Create dirs for log file
+            dirname = os.path.dirname(handler['filename'])
+            if not os.path.isdir(dirname):
+                os.makedirs(dirname)
+
+    logging.config.dictConfig(cfg_dict)
