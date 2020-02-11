a41bea164aee7b64a9021a5b209913309ad2e874
Raymond Wu <raymondwu@fontrip.com>
2020-02-11 12:19:06 +0800

    console_print 改為 logging
    * helper 專用設定
    * 移除 console_print, set_slient
    * setup.py 與 helper.py 改用 logging 顯示訊息
    
    Mentions: #44

--- skcom/samples/setup.py
+++ skcom/samples/setup.py
@@ -1,6 +1,7 @@
 """
 安裝程式範例
 """
+import logging
 from packaging import version
 
 import skcom.helper
@@ -9,35 +10,37 @@ def main():
     """
     安裝流程
     """
+    logger = logging.getLogger('helper')
 
     # 檢查與更新 Visual C++ 2010 (>= 10.0.40219.325)
     required_ver = version.parse('10.0.40219.325')
     current_ver = skcom.helper.verof_vcredist()
     if current_ver < required_ver:
-        print('安裝 Visual C++ 2010 可轉發套件')
+        logger.info('安裝 Visual C++ 2010 可轉發套件')
         skcom.helper.install_vcredist()
         current_ver = skcom.helper.verof_vcredist()
-        if current_ver < required_ver:
-            print('安裝失敗, 可能是您取消了安裝動作')
-            return
+    if current_ver < required_ver:
+        logger.error('安裝失敗, 可能是您取消了安裝動作')
+        return
+    logger.info('Visual C++ 2010 可轉發套件已安裝, 版本: %s', current_ver)
 
-    print('Visual C++ 2010 可轉發套件已安裝, 版本:', current_ver)
-
-    # 檢查與更新 API 元件 (>= 2.13.11)
+    # 檢查與更新 API 元件 (>= 2.13.18)
     skcom_verstr = '2.13.18'
     required_ver = version.parse(skcom_verstr)
     current_ver = skcom.helper.verof_skcom()
     if current_ver < required_ver:
-        print('安裝與註冊群益 API 元件')
+        logger.info('安裝與註冊群益 API 元件')
         skcom.helper.install_skcom(skcom_verstr)
         current_ver = skcom.helper.verof_skcom()
-
-    print('群益 API 元件已安裝, 版本:', current_ver)
+    if current_ver < required_ver:
+        logger.error('安裝失敗, 可能是您取消了安裝動作')
+        return
+    logger.info('群益 API 元件已安裝, 版本: %s', current_ver)
 
     if not skcom.helper.has_valid_mod():
         skcom.helper.generate_mod()
 
-    print('群益 API 元件模組已生成')
+    logger.info('群益 API 元件模組已生成')
 
 if __name__ == '__main__':
     main()
